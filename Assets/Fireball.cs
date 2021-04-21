e3944fbdb2c141550dc281d1255d4a68dd5b2fb5
Kamil Musiał <kmusial40@gmail.com>
2021-04-21 16:05:14 +0200

    bug with fireball

---
+++ Assets/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    private float _speed = 8f;
+    private float _lifeDuration = 2f;
+    private float _lifeTimer;
+
+    private void Start()
+    {
+        _lifeTimer = _lifeDuration;
+        MoveController();
+    }
+
+    private void Update()
+    {
+        MoveController();
+    }
+
+    private void MoveController()
+    {
+        transform.position += transform.forward * (_speed * Time.deltaTime);
+
+        _lifeTimer -= Time.deltaTime;
+
+        if (_lifeTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject fireball;
+    public GameObject monsterHead;
+    public Camera fpsCamera;
+    private CharacterController _characterController; 
+    private float _fireballSpeed = 10f;
+
+    private void Awake()                                           
+    {                                                              
+        _characterController = GetComponent<CharacterController>();
+    }                                                              
+
+    private void Update()
+    {
+        CreateFireBall();
+    }
+
+    private void CreateFireBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireball, monsterHead.transform.position, fpsCamera.transform.rotation);
+        }
+    }
+}
